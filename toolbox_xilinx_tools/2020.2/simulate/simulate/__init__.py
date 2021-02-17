b1f91a38bb406b9716d37fe779a59e92cb714bc3
Erik Anderson <efa@eecs.berkeley.edu>
2021-02-16 23:06:52 -0800

    first draft simulation tool

--- toolbox_xilinx_tools/2020.2/simulate/simulate/__init__.py
+++ toolbox_xilinx_tools/2020.2/simulate/simulate/__init__.py
@@ -20,57 +20,53 @@ from toolbox.utils import *
 from jinja_tool import JinjaTool
 
 
-class XsimTool(JinjaTool):
+class XsimTool(Tool):
     """Xcelium toolbox tool"""
     def __init__(self, db: Database, log: Callable[[str, LogLevel], None]):
         super(XsimTool, self).__init__(db, log)
         # Create binary driver
-        self.xsim = self.get_db(self.get_namespace("simulate"))
-        self.bin = BinaryDriver(self.xsim["bin"])
+        self.sim = self.get_db(self.get_namespace("XsimTool"))
 
     def steps(self) -> List[Callable[[], None]]:
-        return [self.render_sim_tcl, self.run_xrun]
+        return [self.parse_files, self.elaborate_design, self.simulate_design]
 
-    def render_sim_tcl(self):
-        """Generates tcl that will be passed to xrun"""
-        if self.xcelium["top_tcl_template"]:
-            self.add_jinja_templates([self.xcelium["top_tcl_template"]])
-            template = self.xcelium["top_tcl_template"]
-            output = os.path.join(self.get_db('internal.job_dir'),
-                                  Path(template).name)
-            self.render_to_file(template,
-                                output,
-                                xcelium=self.xcelium,
-                                user=self.get_db("user"))
+    def parse_files(self):
+        """Parsing design files with xvhdl and xvlog"""
+        vlog_bin = BinaryDriver("xvlog")
+        for f in self.sim["defines"]:
+            vlog_bin.add_option(f"{Path(f).resolve()}")
+        for f in self.sim["packages"]:
+            vlog_bin.add_option(f"{Path(f).resolve()}")
+        for f in self.sim["test"]:
+            vlog_bin.add_option(f"{Path(f).resolve()}")
+        for f in self.sim["rtl"]:
+            vlog_bin.add_option(f"{Path(f).resolve()}")
+        for d in self.sim["include_dirs"]:
+            vlog_bin.add_option("-include", f"{Path(d).resolve()}")
+        # Execute
+        self.log(vlog_bin.get_execute_string())
+        exec_dir = self.get_db('internal.job_dir')
+        vlog_bin.execute(directory=exec_dir)
 
-    def append_files(self, fnames: List[str]):
-        """Iterates through file and appends valid ones to exec call"""
-        for f in fnames:
-            self.bin.add_option(value=str(Path(f).resolve()))
+    def elaborate_design(self):
+        """Elaborate design with xelab"""
+        elab_bin = BinaryDriver("xelab")
+        elab_bin.add_option(f"work.{self.sim['testbench']}")
+        elab_bin.add_option("-snapshot", self.sim['testbench'])
+        # Execute
+        self.log(elab_bin.get_execute_string())
+        exec_dir = self.get_db('internal.job_dir')
+        elab_bin.execute(directory=exec_dir)
 
-    def run_xrun(self):
-        """Calls xrun"""
-        # Append files
-        self.append_files(self.xcelium["defines"])
-        self.append_files(self.xcelium["packages"])
-        self.append_files(self.xcelium["rtl"])
-        self.append_files(self.xcelium["test"])
-        # include dirs
-        if self.xcelium["include_dirs"]:
-            incdirs = [
-                str(Path(d).resolve()) for d in self.xcelium["include_dirs"]
-            ]
-            self.bin.add_option(flag="-incdir", value=" ".join(incdirs))
+    def simulate_design(self):
+        """Simulate design with xsim"""
+        sim_bin = BinaryDriver("xsim")
         # Append options raw
-        for o in self.xcelium["options"]:
-            self.bin.add_option(value=o)
-        self.bin.add_option(flag='-top', value=self.xcelium["testbench"])
-        # Append TCL if it exists
-        if self.xcelium["top_tcl_template"]:
-            template = self.xcelium["top_tcl_template"]
-            sim_tcl = os.path.join(self.get_db('internal.job_dir'),
-                                   Path(template).name)
-            self.bin.add_option(flag='-input', value=sim_tcl)
-        # Execute binary
-        self.log(self.bin.get_execute_string())
-        self.bin.execute(directory=self.get_db('internal.job_dir'))
+        for o in self.sim["options"]:
+            sim_bin.add_option(value=o)
+        sim_bin.add_option(self.sim["testbench"])
+        sim_bin.add_option("-runall")
+        # Execute
+        self.log(sim_bin.get_execute_string())
+        exec_dir = self.get_db('internal.job_dir')
+        sim_bin.execute(directory=exec_dir)
